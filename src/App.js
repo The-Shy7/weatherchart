65f6ef790bf49382fb15f6eafc26a5f292b047dd
tahmit <tahmit@uw.edu>
2020-02-05 09:16:09 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Input, Button} from 'antd'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
     </div>
   );
+}
+
+function Header() {
+  const [text, setText] = useState('')
+
+  return <header className="App-header">
+    <Input 
+      style={{height:'3rem',fontSize:'2rem'}}
+    />
+    <Button style={{marginLeft:5,height:'3rem'}}>
+      Search
+    </Button>
+  </header>
 }
 
 export default App;
