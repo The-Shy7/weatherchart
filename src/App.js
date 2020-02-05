eeab10d9fbfe6aeed461f80e91a6df75591ede10
tahmit <tahmit@uw.edu>
2020-02-05 09:27:21 -0800

    added useContext functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import './App.css';
 import {Input, Button} from 'antd'
 
 const context = React.createContext()
 
 function App() {
-  return (
+  const [state, setState] = useState({})
+
+  return <context.Provider value={{
+    ...state,
+    set: v=>setState({...state, ...v})
+  }}>
+  
     <div className="App">
       <Header/>
     </div>
-  );
+  </context.Provider>
 }
 
 function Header() {
+  const ctx = useContext(context)
+
   return <header className="App-header">
     <Input 
       style={{height:'3rem',fontSize:'2rem'}}
