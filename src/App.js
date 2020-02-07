b56d763fd383c0128c73ae014d09d48fd2aea480
tahmit <tahmit@uw.edu>
2020-02-07 08:52:35 -0800

    Added Bar to body, fixed fetch request code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useContext, useState} from 'react';
-import './App.css';
+import React, {useContext, useState} from 'react'
+import './App.css'
 import {Input, Button} from 'antd'
 import {Bar} from 'react-chartjs-2'
 
@@ -7,69 +7,78 @@ const context = React.createContext()
 
 function App() {
   const [state, setState] = useState({
-    searchTerm:''
+    searchTerm:'',
   })
-
   return <context.Provider value={{
     ...state,
-    set: v=>setState({...state, ...v})
+    set: v=> setState({...state, ...v})
   }}>
-  
     <div className="App">
-      <Header/>
-      <Body/>
+      <Header />
+      <Body />
     </div>
   </context.Provider>
 }
 
-function Header() {
+function Header(){
   const ctx = useContext(context)
-
   return <header className="App-header">
     <Input 
       value={ctx.searchTerm}
-      onChange={e => ctx.set({searchTerm: e.target.value})}
-      style={{height:'3rem',fontSize:'2rem'}}
+      onChange={e=> ctx.set({searchTerm: e.target.value})}
+      style={{height:'3rem',fontSize:'2rem'}} 
       onKeyPress={e=>{
-        if (e.key === 'Enter' && ctx.searchTerm) search(ctx)
+        if(e.key==='Enter' && ctx.searchTerm) search(ctx)
       }}
     />
     <Button style={{marginLeft:5,height:'3rem'}}
-      onClick={() => search(ctx)} type="primary"
+      onClick={()=> search(ctx)} type="primary"
       disabled={!ctx.searchTerm}>
       Search
     </Button>
   </header>
 }
 
-function Body() {
+function Body(){
   const ctx = useContext(context)
-  const data = ctx.weather && 
-
+  const {error, weather} = ctx
+  let data
+  if(weather){
+    console.log(weather)
+    data = {
+      labels: weather.hourly.data.map(d=>d.time),
+      datasets: [{
+        label:'Temperature',
+        data: weather.hourly.data.map(d=>d.temperature)
+      }]
+    }
+  }
+  console.log(data)
   return <div className="App-body">
-    {ctx.error && <div className="error">{ctx.error}</div>}
-    {ctx.weather && <div>
-    
+    {error && <div className="error">{error}</div>}
+    {data && <div>
+      <Bar data={data}
+        width={800} height={400}
+      />
     </div>}
   </div>
 }
 
-async function search({searchTerm, set}) {
+async function search({searchTerm, set}){
   try {
     const term = searchTerm
     set({searchTerm:'', error:''})
-  
-    const osmurl = `https://nominatim.openstreetmap.org/search/${searchTerm}?format=json`
+
+    const osmurl = `https://nominatim.openstreetmap.org/search/${term}?format=json`
     const r = await fetch(osmurl)
     const loc = await r.json()
-  
-    if (!loc[0]) {
+    if(!loc[0]){
       return set({error:'No city matching that query'})
     }
-  
     const city = loc[0]
-    console.log(city.lat, city.lon)
-    const key = 'ff44717dec09b51014ff551f271f55ed'
+
+    //const url = `/api?lat=${city.lat}&lon=${city.lon}`
+    const key = '45236e8510745ee86684a5946eda8cda'
     const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${city.lat},${city.lon}`
     const r2 = await fetch(url)
     const weather = await r2.json()
